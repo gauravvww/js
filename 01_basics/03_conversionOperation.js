cc21e6a3cf3bbdb59f993037cb21c9610647b9be
gauravvww <gaurav1130pm@gmail.com>
2024-06-25 01:54:11 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "gaurav"
+/*
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn= "gaurav"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn )
+console.log(booleanIsLoggedIn)
+*/
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
